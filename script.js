92c27a0bdd84b55890aafff1d9a0abc3247445b8
Sofronov Semyon <sofronov.semyon@gmail.com>
2020-03-20 22:24:45 +0400

    feat: finish dom events

--- script.js
+++ script.js
@@ -6,6 +6,33 @@ document.querySelector('.header__navigation-panel').addEventListener('click', (e
             item.classList.remove('navigation-panel__text_active');
         });
         target.classList.add('navigation-panel__text_active');
+
+        const text = target.innerHTML;
+        switch (text) {
+            case 'Home': {
+                slider.scrollIntoView({behavior: "smooth"});
+                break;
+            }
+            case 'Services': {
+                window.scrollTo({top: serviceSection.offsetTop - 95, behavior: "smooth"});
+                break;
+            }
+            case 'Portfolio': {
+                window.scrollTo({top: portfolioSection.offsetTop - 95, behavior: "smooth"});
+                break;
+            }
+            case 'About': {
+                window.scrollTo({top: aboutSection.offsetTop - 95, behavior: "smooth"});
+                break;
+            }
+            case 'Contact': {
+                window.scrollTo({top: contactInfo.offsetTop - 95, behavior: "smooth"});
+                break;
+            }
+            default: {
+                window.scrollTo({top: 0, behavior: "smooth"});
+            }
+        }
     }
 });
 
@@ -39,72 +66,24 @@ document.querySelector('.grid').addEventListener('click', (event) => {
     }
 });
 
-let isDisplay = true;
-const slider = document.querySelector('.slider');
 const iphoneVertical = document.querySelector('.slider__iphone-vertical');
 const iphoneHorizontal = document.querySelector('.slider__iphone-horizontal');
-const iphoneLeft = document.querySelector('.slider__iphone-left');
-const iphoneCenter = document.querySelector('.slider__iphone-center');
-const iphoneRight = document.querySelector('.slider__iphone-right');
-
-const sliderContent = document.querySelector('.slider__content');
-let backgroundVertical = document.createElement('img');
-backgroundVertical.setAttribute('src', 'assets/Background.png');
-backgroundVertical.style.zIndex = '1000';
-backgroundVertical.style.display = 'none';
-backgroundVertical.style.position = 'absolute';
-backgroundVertical.style.top = '213px';
-backgroundVertical.style.left = '126px';
-sliderContent.append(backgroundVertical);
-let isVisibleVerticalBackground = false;
-
-let backgroundHorizontal = document.createElement('img');
-backgroundHorizontal.setAttribute('src', 'assets/Background.png');
-backgroundHorizontal.style.zIndex = '1000';
-backgroundHorizontal.style.display = 'none';
-backgroundHorizontal.style.position = 'absolute';
-backgroundHorizontal.style.top = '210px';
-backgroundHorizontal.style.left = '558px';
-backgroundHorizontal.style.transform = 'rotate(90deg)';
-sliderContent.append(backgroundHorizontal);
-let isVisibleHorizontalBackground = false;
-
-const arrowClickHandler = () => {
-    if (isDisplay) {
-        isDisplay = false;
-        slider.style.background = '#648BF0';
-        slider.style.borderBottom = '#648BF0 6px solid';
-        iphoneVertical.style.display = 'none';
-        iphoneHorizontal.style.display = 'none';
-        iphoneLeft.style.display = 'block';
-        iphoneCenter.style.display = 'block';
-        iphoneRight.style.display = 'block';
-        backgroundVertical.style.display = 'none';
-        backgroundHorizontal.style.display = 'none';
-        isVisibleVerticalBackground = false;
-        isVisibleHorizontalBackground = false;
-    } else {
-        isDisplay = true;
-        slider.style.background = '#f06c64';
-        slider.style.borderBottom = '#ea676b 6px solid';
-        iphoneVertical.style.display = 'block';
-        iphoneHorizontal.style.display = 'block';
-        iphoneLeft.style.display = 'none';
-        iphoneCenter.style.display = 'none';
-        iphoneRight.style.display = 'none';
-    }
-};
-document.querySelector('.slider__arrow_right').addEventListener('click', arrowClickHandler);
-document.querySelector('.slider__arrow_left').addEventListener('click', arrowClickHandler);
-
 
 iphoneVertical.addEventListener('click', () => {
-    if (!isVisibleVerticalBackground) {
-        isVisibleVerticalBackground = true;
-        backgroundVertical.style.display = 'block';
+    let verticalBackground = document.querySelector('.slider__background');
+    if (verticalBackground.classList.contains('slider__background_active')) {
+        verticalBackground.classList.remove('slider__background_active');
     } else {
-        isVisibleVerticalBackground = false;
-        backgroundVertical.style.display = 'none';
+        verticalBackground.classList.add('slider__background_active');
+    }
+});
+
+iphoneHorizontal.addEventListener('click', () => {
+    let horizontalBackground = document.querySelector('.slider__background_horizontal');
+    if (horizontalBackground.classList.contains('slider__background_active')) {
+        horizontalBackground.classList.remove('slider__background_active');
+    } else {
+        horizontalBackground.classList.add('slider__background_active');
     }
 });
 
@@ -118,24 +97,84 @@ iphoneHorizontal.addEventListener('click', () => {
     }
 });
 
+let slides = [];
+slides = document.querySelectorAll('.slider__content');
+let currentSlideIndex = 0;
+let isEnabled = true;
+
+const switchSlide = (index) => currentSlideIndex = (index + slides.length) % slides.length;
+
+const nextSlide  = (index) => {
+    hideSlide('to-left');
+    switchSlide(index + 1);
+    showSlide('from-right');
+};
+
+const prevSlide  = (index) => {
+    hideSlide('to-right');
+    switchSlide(index - 1);
+    showSlide('from-left');
+};
+
+const hideSlide = (direction) => {
+    isEnabled = false;
+    const slide = slides[currentSlideIndex];
+    slide.classList.add(direction);
+    slide.addEventListener('animationend', function () {
+        this.classList.remove('slider__content_active', direction);
+    })
+};
+
+const showSlide = (direction) => {
+    const slide = slides[currentSlideIndex];
+    slide.classList.add('slider__content_next', direction);
+    slide.addEventListener('animationend', function () {
+        this.classList.remove('slider__content_next', direction);
+        this.classList.add('slider__content_active');
+        isEnabled = true;
+    })
+};
+
+document.querySelector('.slider__arrow_right').addEventListener('click', () => {
+    if (isEnabled) {
+        nextSlide(currentSlideIndex);
+    }
+});
+
+document.querySelector('.slider__arrow_left').addEventListener('click', () => {
+    if (isEnabled) {
+        prevSlide(currentSlideIndex);
+    }
+});
+
 let form = document.querySelector('form');
 form.onsubmit = (event) => {
     event.preventDefault();
     let elements = event.target.elements;
-    let result = 'Письмо отправлено\n';
+    let result = `Письмо отправлено<br>`;
 
     let subjectValue = elements.subject.value;
     if (subjectValue.trim() === '') {
-        result += 'Без темы\n';
+        result += `Без темы<br>`;
     } else {
-        result += `Тема: ${subjectValue}\n`;
+        result += `Тема: ${subjectValue}<br>`;
     }
 
     let descriptionValue = elements.description.value;
     if (descriptionValue.trim() === '') {
-        result += 'Без описания\n';
+        result += `Без описания<br>`;
     } else {
-        result += `Описание: ${descriptionValue}\n`;
+        result += `Описание: ${descriptionValue}<br>`;
     }
-    alert(result);
+
+    let dialog = document.querySelector('.modal-dialog');
+    dialog.classList.add('modal-dialog_active');
+    let dialogText = document.querySelector('.modal-dialog__text');
+    dialogText.innerHTML = result;
+
+    modalBtn.addEventListener('click', () => {
+        dialog.classList.remove('modal-dialog_active');
+        form.reset();
+    })
+
 };
